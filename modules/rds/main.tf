f2b46d972f4427c2f59e34595ff81ad0ff1ce59b
Aashish Gupta <aashish.gupta@sourcefuse.com>
2024-05-16 08:59:38 +0530

    Add RDS Module

---
+++ modules/rds/main.tf
@@ -0,0 +1,65 @@
+# Generate a random password with lower case, upper case, numbers, and special characters
+resource "random_password" "db_password" {
+  length           = 16
+  special          = true
+  override_special = "!@#$%^&*()-_=+[]{}<>:?"
+}
+
+# Store the generated password in SSM Parameter Store
+resource "aws_ssm_parameter" "db_password" {
+  name  = var.db_password_parameter_name
+  type  = "SecureString"
+  value = random_password.db_password.result
+}
+
+# Conditionally create Subnet Group if var.use_existing_subnet_group is false
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  count      = var.use_existing_subnet_group ? 0 : 1
+  name       = var.db_subnet_group_name
+  subnet_ids = var.subnet_ids
+}
+
+# Conditionally create Parameter Group if var.use_existing_subnet_group is false
+resource "aws_db_parameter_group" "rds_parameter_group" {
+  count       = var.use_existing_parameter_group ? 0 : 1
+  name        = var.db_parameter_group_name
+  family      = var.db_parameter_group_family
+  description = "Custom parameter group for ${var.db_engine}"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_db_instance" "arc_rds" {
+
+  engine                 = var.db_engine
+  engine_version         = var.db_engine_version
+  db_name                = var.db_name
+  identifier             = var.db_identifier
+  instance_class         = var.db_instance_class
+  allocated_storage      = var.db_allocated_storage
+  storage_type           = var.db_storage_type
+  storage_encrypted      = true
+  max_allocated_storage  = var.db_max_allocated_storage
+  publicly_accessible    = var.db_publicly_accessible
+  username               = var.db_username
+  password               = aws_ssm_parameter.db_password.value
+  vpc_security_group_ids = var.vpc_security_group_ids
+  skip_final_snapshot    = var.skip_final_snapshot
+  db_subnet_group_name   = var.use_existing_subnet_group ? var.db_subnet_group_name : aws_db_subnet_group.rds_subnet_group[0].name
+  parameter_group_name   = var.use_existing_parameter_group ? var.db_parameter_group_name : aws_db_parameter_group.rds_parameter_group[0].name
+  apply_immediately      = true
+  tags = {
+    Name = "${var.db_identifier}"
+  }
+}
+
+
+
+
---
+++ modules/rds/variables.tf
@@ -0,0 +1,132 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "db_engine" {
+  description = "The database engine type"
+  type        = string
+  default     = "postgres"
+}
+
+variable "db_engine_version" {
+  description = "The database engine version to use"
+  type        = string
+  default     = "16.3R1"
+}
+
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+  default     = "example"
+}
+
+variable "db_identifier" {
+  description = "The identifier for the RDS instance"
+  type        = string
+  default     = "example"
+}
+
+variable "db_instance_class" {
+  description = "The RDS instance class"
+  type        = string
+  default     = "db.t2.micro"
+}
+
+
+variable "db_publicly_accessible" {
+  description = "Whether the RDS instance is publicly accessible"
+  type        = bool
+  default     = true
+}
+
+variable "db_username" {
+  description = "The master username for the database"
+  type        = string
+}
+
+
+variable "vpc_security_group_ids" {
+  description = "List of security group IDs for the RDS instance"
+  type        = list(string)
+}
+
+variable "skip_final_snapshot" {
+  description = "Whether to skip the final DB snapshot during termination"
+  type        = bool
+  default     = true
+}
+
+variable "db_subnet_group_name" {
+  description = "The name of the DB subnet group"
+  type        = string
+  default     = "my-db-subnet-group"
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the RDS instance"
+  type        = list(string)
+}
+
+
+variable "db_allocated_storage" {
+  description = "The amount of storage (in GB) to be initially allocated for the database"
+  type        = number
+  default     = 20
+}
+
+variable "db_storage_type" {
+  description = "The storage type to be associated with the RDS instance. Valid values are 'standard', 'gp2', or 'io1'."
+  type        = string
+  default     = "gp2"
+}
+
+
+variable "db_max_allocated_storage" {
+  description = "The upper limit to which Amazon RDS can automatically scale the storage of the DB instance."
+  type        = number
+  default     = 100
+}
+
+
+variable "use_existing_subnet_group" {
+  description = "Flag to determine whether to use an existing subnet group"
+  type        = bool
+  default     = false
+}
+
+variable "db_password_parameter_name" {
+  description = "The name of the SSM parameter that stores the database password"
+  type        = string
+  default     = null
+}
+
+variable "db_parameter_group_name" {
+  description = "The name of the DB parameter group"
+  type        = string
+  default     = "my-db-parameter-group"
+}
+
+variable "db_parameter_group_family" {
+  description = "The family of the DB parameter group"
+  type        = string
+  default     = null
+}
+
+variable "use_existing_parameter_group" {
+  description = "Flag to determine whether to use an existing parameter group"
+  type        = bool
+  default     = false
+}
+
+variable "storage_encrypted" {
+  description = "Flag to determine whether the DB instance is encrypted."
+  type        = bool
+  default     = false
+}
+
+
+
+
+
+
